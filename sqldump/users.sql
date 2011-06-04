82195f4045fdd4f917a69d3bd7e528ac5be034b3
joseph <rehtaew@gmail.com>
2011-06-03 19:32:03 -0500

    Defaults on ping, users tables.

--- sqldump/users.sql
+++ sqldump/users.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `{prefix}users` (
   `userid` int(10) NOT NULL,
   `settings` int(10) NOT NULL DEFAULT '32',
   `defaultRoom` int(10) NOT NULL DEFAULT '0',
-  `favRooms` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `favRooms` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '1',
   `watchRooms` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `status` int(3) NOT NULL,
   `defaultFormatting` int(10) NOT NULL,
