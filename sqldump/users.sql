57ebb623bafa5f6ce3a41bf4e009980e1193c499
joseph <rehtaew@gmail.com>
2011-06-08 14:09:33 -0500

    Gonna start trying to reduce queries to forum tables; here, adding userName, userGroup, and allGroups to users.sql.

--- sqldump/users.sql
+++ sqldump/users.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS `{prefix}users` (
   `userId` int(10) NOT NULL,
   `userName` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `userGroup` int(10) NOT NULL DEFAULT 1
+  `allGroups` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `defaultRoom` int(10) NOT NULL DEFAULT 1,
   `favRooms` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '1',
   `watchRooms` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
