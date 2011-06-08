0d6f0df05423094171200b522ed74715e8581fb8
joseph <rehtaew@gmail.com>
2011-06-07 20:06:06 -0500

    Kick to use roomId, normal int

--- sqldump/kick.sql
+++ sqldump/kick.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS `{prefix}kick` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `userId` int(10) NOT NULL,
+  `roomId` int(10) NOT NULL,
   `kickerid` int(10) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `length` bigint(20) NOT NULL,
-  `room` int(10) NOT NULL,
+  `length` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
-  KEY `room` (`room`),
+  KEY `roomId` (`roomId`),
   KEY `time` (`time`),
   KEY `length` (`length`)
 ) ENGINE=MEMORY  DEFAULT CHARSET=utf8;
