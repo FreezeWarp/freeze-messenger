4bd29c1f5531aa79f9e0d20235cfa1f19fbedb8a
joseph <rehtaew@gmail.com>
2011-06-09 03:53:15 -0500

    Fix sqldump: phrases, rooms

--- sqldump/rooms.sql
+++ sqldump/rooms.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `{prefix}rooms` (
   `roomId` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  topic` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `topic` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `allowedGroups` TEXT(10000) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'Comma-separated',
   `allowedUsers` TEXT(10000) NOT NULL COMMENT 'Comma-separated',
   `owner` int(10) NOT NULL,
@@ -19,5 +19,5 @@ CREATE TABLE IF NOT EXISTS `{prefix}rooms` (
 
 -- DIVIDE
 
-INSERT INTO `{prefix}rooms` (`roomId`, `name`, `title`, `allowedGroups`, `allowedUsers`, `options`) VALUES
+INSERT INTO `{prefix}rooms` (`roomId`, `name`, `topic`, `allowedGroups`, `allowedUsers`, `options`) VALUES
 (1, 'Your Room!', 'Hit the Edit Room Button to Change Things or Use /topic to Change the Topic', '*', '*', 1)
