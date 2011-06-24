89e17d47fa18c197352b861fc0f14e59bd8ef96e
joseph <rehtaew@gmail.com>
2011-06-23 22:45:56 -0500

    Revert context menu change; we'll do that specific one the old fashioned way (since it doesn't show the same leaks especially).

--- interface/client/js/jquery.plugins.js
+++ interface/client/js/jquery.plugins.js
@@ -565,350 +565,160 @@ if(jQuery)(function() {
       if (o.inSpeed == 0) o.inSpeed = -1;
       if (o.outSpeed == 0) o.outSpeed = -1;
 
-      $(this).die('mousedown');
-      $(this).live({
-        /*mousedown : function() { // Loop each context menu
-          if (event.which == 3) {
-            var offset = $(this).offset();
+      $(this).each(function() { // Loop each context menu
+        var el = $(this);
+        var offset = $(el).offset();
 
-            $('#' + o.menu).addClass('contextMenu'); // Add contextMenu class
-          }
-        },*/
-        mouseup : function() {
-//          $(this).stopPropagation();
-          var srcElement = $(this);
-alert(1);
-          if (event.button == 3) { alert(3);
-            $(".contextMenu").hide(); // Hide context menus that may be showing
+        $('#' + o.menu).addClass('contextMenu'); // Add contextMenu class
 
-            var menu = $('#' + o.menu); // Get this context menu
+        $(this).mousedown(function(e) { // Simulate a true right clickasync
+          var evt = e;
+          evt.stopPropagation();
 
-            // Detect mouse position
-            var d = {}, x, y;
-            if (self.innerHeight) {
-              d.pageYOffset = self.pageYOffset;
-              d.pageXOffset = self.pageXOffset;
-              d.innerHeight = self.innerHeight;
-              d.innerWidth = self.innerWidth;
-            }
-            else if (document.documentElement && document.documentElement.clientHeight) {
-              d.pageYOffset = document.documentElement.scrollTop;
-              d.pageXOffset = document.documentElement.scrollLeft;
-              d.innerHeight = document.documentElement.clientHeight;
-              d.innerWidth = document.documentElement.clientWidth;
-            }
-            else if (document.body) {
-              d.pageYOffset = document.body.scrollTop;
-              d.pageXOffset = document.body.scrollLeft;
-              d.innerHeight = document.body.clientHeight;
-              d.innerWidth = document.body.clientWidth;
-            }
+          $(this).mouseup(function(e) {
+            e.stopPropagation();
+            var srcElement = $(this);
+            $(this).unbind('mouseup');
 
-            x = (e.pageX ? e.pageX : e.clientX + d.scrollLeft);
-            y = (e.pageY ? e.pageY : e.clientY + d.scrollTop);
+            if (evt.button == 2) {
+              $(".contextMenu").hide(); // Hide context menus that may be showing
 
+              var menu = $('#' + o.menu); // Get this context menu
 
-            // Show the menu
-            //$(document).die('click');
-            $(menu).css({ top: y, left: x }).fadeIn(o.inSpeed);
+              if ($(el).hasClass('disabled')) return false;
 
+              // Detect mouse position
+              var d = {}, x, y;
+              if (self.innerHeight) {
+                d.pageYOffset = self.pageYOffset;
+                d.pageXOffset = self.pageXOffset;
+                d.innerHeight = self.innerHeight;
+                d.innerWidth = self.innerWidth;
+              }
+              else if (document.documentElement && document.documentElement.clientHeight) {
+                d.pageYOffset = document.documentElement.scrollTop;
+                d.pageXOffset = document.documentElement.scrollLeft;
+                d.innerHeight = document.documentElement.clientHeight;
+                d.innerWidth = document.documentElement.clientWidth;
+              }
+              else if (document.body) {
+                d.pageYOffset = document.body.scrollTop;
+                d.pageXOffset = document.body.scrollLeft;
+                d.innerHeight = document.body.clientHeight;
+                d.innerWidth = document.body.clientWidth;
+              }
 
-            // Hover events
-            $(menu).find('a').mouseover(function() {
-              $(menu).find('li.hover').removeClass('hover');
-              $(this).parent().addClass('hover');
-            }).mouseout(function() {
-              $(menu).find('li.hover').removeClass('hover');
-            });
+              x = (e.pageX ? e.pageX : e.clientX + d.scrollLeft);
+              y = (e.pageY ? e.pageY : e.clientY + d.scrollTop);
 
+              // Show the menu
+              $(document).unbind('click');
+              $(menu).css({ top: y, left: x }).fadeIn(o.inSpeed);
 
-            // Keyboard
-            $(document).keypress(function(e) {
-              switch(e.keyCode) {
-                case 38: // up
-                if ($(menu).find('li.hover').size() == 0) {
-                  $(menu).find('li:last').addClass('hover');
-                }
-                else {
-                  $(menu).find('li.hover').removeClass('hover').prevAll('li:not(.disabled)').eq(0).addClass('hover');
+              // Hover events
+              $(menu).find('a').mouseover(function() {
+                $(menu).find('li.hover').removeClass('hover');
+                $(this).parent().addClass('hover');
+              }).mouseout(function() {
+                $(menu).find('li.hover').removeClass('hover');
+              });
 
+              // Keyboard
+              $(document).keypress(function(e) {
+                switch(e.keyCode) {
+                  case 38: // up
                   if ($(menu).find('li.hover').size() == 0) {
                     $(menu).find('li:last').addClass('hover');
                   }
-                }
-                break;
+                  else {
+                    $(menu).find('li.hover').removeClass('hover').prevAll('li:not(.disabled)').eq(0).addClass('hover');
 
-                case 40: // down
-                if ($(menu).find('li.hover').size() == 0) {
-                  $(menu).find('li:first').addClass('hover');
-                }
-                else {
-                  $(menu).find('li.hover').removeClass('hover').nextAll('li:not(.disabled)').eq(0).addClass('hover');
+                    if ($(menu).find('li.hover').size() == 0) {
+                      $(menu).find('li:last').addClass('hover');
+                    }
+                  }
+                  break;
 
+                  case 40: // down
                   if ($(menu).find('li.hover').size() == 0) {
                     $(menu).find('li:first').addClass('hover');
                   }
+                  else {
+                    $(menu).find('li.hover').removeClass('hover').nextAll('li:not(.disabled)').eq(0).addClass('hover');
+
+                    if ($(menu).find('li.hover').size() == 0) {
+                      $(menu).find('li:first').addClass('hover');
+                    }
+                  }
+                  break;
+
+                  case 13: // enter
+                  $(menu).find('li.hover a').trigger('click');
+                  break;
+
+                  case 27: // esc
+                  $(document).trigger('click');
+                  break
                 }
-                break;
+              });
 
-                case 13: // enter
-                $(menu).find('li.hover a').trigger('click');
-                break;
-
-                case 27: // esc
-                $(document).trigger('click');
-                break
-              }
-            });
-
-            // When items are selected
-            $('#' + o.menu).find('a').unbind('click');
-            $('#' + o.menu).find('li:not(.disabled) a').click(function() {
-              $(document).unbind('click').unbind('keypress');
-              $(".contextMenu").hide();
-
-              // Callback
-              if (callback) {
-                callback($(this).attr('data-action'), $(srcElement), {x: x - offset.left, y: y - offset.top, docX: x, docY: y});
-              }
-
-              return false;
-            });
-
-            // Hide bindings
-            setTimeout(function() { // Delay for Mozilla; TODO: Confirm still a problem
-              $(document).click(function() {
+              // When items are selected
+              $('#' + o.menu).find('a').unbind('click');
+              $('#' + o.menu).find('li:not(.disabled) a').click(function() {
                 $(document).unbind('click').unbind('keypress');
-                $(menu).fadeOut(o.outSpeed);
+                $(".contextMenu").hide();
+
+                // Callback
+                if (callback) {
+                  callback($(this).attr('data-action'), $(srcElement), {x: x - offset.left, y: y - offset.top, docX: x, docY: y});
+                }
+
                 return false;
               });
-            }, 0);
 
-            // Disable text selection
-            if ($.browser.mozilla) {
-              $('#' + o.menu).each(function() { $(this).css({ 'MozUserSelect' : 'none' }); });
+              // Hide bindings
+              setTimeout(function() { // Delay for Mozilla; TODO: Confirm still a problem
+                $(document).click(function() {
+                  $(document).unbind('click').unbind('keypress');
+                  $(menu).fadeOut(o.outSpeed);
+                  return false;
+                });
+              }, 0);
             }
-            else if ($.browser.msie) {
-              $('#' + o.menu).each(function() { $(this).bind('selectstart.disableTextSelect', function() { return false; }); });
-            }
-            else {
-              $('#' + o.menu).each(function() { $(this).bind('mousedown.disableTextSelect', function() { return false; }); });
-            }
+          });
+        });
 
-            $(this).add($('ul.contextMenu')).bind('contextmenu', function() { return false; }); // Disable browser context menu (requires both selectors to work in IE/Safari + FF/Chrome)
-          }
+        // Disable text selection
+        if ($.browser.mozilla) {
+          $('#' + o.menu).each(function() { $(this).css({ 'MozUserSelect' : 'none' }); });
         }
+        else if ($.browser.msie) {
+          $('#' + o.menu).each(function() { $(this).bind('selectstart.disableTextSelect', function() { return false; }); });
+        }
+        else {
+          $('#' + o.menu).each(function() { $(this).bind('mousedown.disableTextSelect', function() { return false; }); });
+        }
+
+        // Disable browser context menu (requires both selectors to work in IE/Safari + FF/Chrome)
+        $(el).add($('UL.contextMenu')).bind('contextmenu', function() { return false; });
+
       });
 
       return $(this);
     },
 
-    destroyContextMenu: function() { // Destroy context menu(s)
-      $(this).each(function() { // Destroy specified context menus
-        $(this).unbind('mousedown').unbind('mouseup'); // Disable action
+    // Destroy context menu(s)
+    destroyContextMenu: function() {
+      // Destroy specified context menus
+      $(this).each(function() {
+        // Disable action
+        $(this).unbind('mousedown').unbind('mouseup');
       });
 
       return($(this));
     }
 
   });
-})(jQuery);
-
-/* END jQuery Context Menu */
-
-
-
-
-
-
-
-
-
-
-/* START jQuery Tooltip #1 */
-
- /*
- * TipTip (Original)
- * Copyright 2010 Drew Wilson
- * www.drewwilson.com
- * code.drewwilson.com/entry/tiptip-jquery-plugin
- *
- * Version 1.3   -   Updated: Mar. 23, 2010
- *
- * This Plug-In will create a custom tooltip to replace the default
- * browser tooltip. It is extremely lightweight and very smart in
- * that it detects the edges of the browser window and will make sure
- * the tooltip stays within the current window size. As a result the
- * tooltip will adjust itself to be displayed above, below, to the left
- * or to the right depending on what is necessary to stay within the
- * browser window. It is completely customizable as well via CSS.
- *
- * This TipTip jQuery plug-in is dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- */
-
-/* TipTip (Modified)
- * Version 1.3 Modified June 26th, 2011
- * Joseph T. Parsons
- *
- * Modified to support live() handler, eliminate code unneeded for FIM (e.g. click handlers). */
-
-(function($) {
-  $.fn.tipTip = function(options) {
-    var defaults = {
-      maxWidth: "200px",
-      edgeOffset: 3,
-      defaultPosition: "bottom",
-      delay: 400,
-      fadeIn: 200,
-      fadeOut: 200,
-      attribute: "title",
-      content: false, // HTML or String to fill TipTIp with
-      enter: function() {return false;},
-      exit: function() {return false;}
-    };
-    var opts = $.extend(defaults, options);
-
-    // Setup tip tip elements and render them to the DOM
-    if ($("#tiptip_holder").length <= 0) {
-      var tiptip_holder = $('<div id="tiptip_holder" style="max-width:'+ opts.maxWidth +';"></div>');
-      var tiptip_content = $('<div id="tiptip_content"></div>');
-      var tiptip_arrow = $('<div id="tiptip_arrow"></div>');
-      $("body").append(tiptip_holder.html(tiptip_content).prepend(tiptip_arrow.html('<div id="tiptip_arrow_inner"></div>')));
-    }
-    else {
-      var tiptip_holder = $("#tiptip_holder");
-      var tiptip_content = $("#tiptip_content");
-      var tiptip_arrow = $("#tiptip_arrow");
-    }
-
-
-
-    var timeout = false;
-
-    $(this).live({
-      mouseover : function() {
-        if (opts.content) {
-          var org_title = opts.content;
-        }
-        else {
-          var org_title = $(this).attr(opts.attribute);
-        }
-
-        opts.enter.call(this);
-        tiptip_content.html(org_title);
-        tiptip_holder.hide().removeAttr("class").css("margin","0");
-        tiptip_arrow.removeAttr("style");
-
-        var top = parseInt($(this).offset()['top']);
-        var left = parseInt($(this).offset()['left']);
-        var org_width = parseInt($(this).outerWidth());
-        var org_height = parseInt($(this).outerHeight());
-        var tip_w = tiptip_holder.outerWidth();
-        var tip_h = tiptip_holder.outerHeight();
-        var w_compare = Math.round((org_width - tip_w) / 2);
-        var h_compare = Math.round((org_height - tip_h) / 2);
-        var marg_left = Math.round(left + w_compare);
-        var marg_top = Math.round(top + org_height + opts.edgeOffset);
-        var t_class = "";
-        var arrow_top = "";
-        var arrow_left = Math.round(tip_w - 12) / 2;
-
-        if (opts.defaultPosition == "bottom") {
-          t_class = "_bottom";
-        }
-        else if (opts.defaultPosition == "top") {
-          t_class = "_top";
-        }
-        else if (opts.defaultPosition == "left") {
-          t_class = "_left";
-        }
-        else if (opts.defaultPosition == "right") {
-          t_class = "_right";
-        }
-
-        var right_compare = (w_compare + left) < parseInt($(window).scrollLeft());
-        var left_compare = (tip_w + left) > parseInt($(window).width());
-
-        if ((right_compare && w_compare < 0) || (t_class == "_right" && !left_compare) || (t_class == "_left" && left < (tip_w + opts.edgeOffset + 5))) {
-          t_class = "_right";
-          arrow_top = Math.round(tip_h - 13) / 2;
-          arrow_left = -12;
-          marg_left = Math.round(left + org_width + opts.edgeOffset);
-          marg_top = Math.round(top + h_compare);
-        }
-        else if ((left_compare && w_compare < 0) || (t_class == "_left" && !right_compare)) {
-          t_class = "_left";
-          arrow_top = Math.round(tip_h - 13) / 2;
-          arrow_left =  Math.round(tip_w);
-          marg_left = Math.round(left - (tip_w + opts.edgeOffset + 5));
-          marg_top = Math.round(top + h_compare);
-        }
-
-        var top_compare = (top + org_height + opts.edgeOffset + tip_h + 8) > parseInt($(window).height() + $(window).scrollTop());
-        var bottom_compare = ((top + org_height) - (opts.edgeOffset + tip_h + 8)) < 0;
-
-        if (top_compare || (t_class == "_bottom" && top_compare) || (t_class == "_top" && !bottom_compare)) {
-          if (t_class == "_top" || t_class == "_bottom") {
-            t_class = "_top";
-          }
-          else {
-            t_class = t_class+"_top";
-          }
-          arrow_top = tip_h;
-          marg_top = Math.round(top - (tip_h + 5 + opts.edgeOffset));
-        }
-        else if (bottom_compare | (t_class == "_top" && bottom_compare) || (t_class == "_bottom" && !top_compare)) {
-          if (t_class == "_top" || t_class == "_bottom") {
-            t_class = "_bottom";
-          }
-          else {
-            t_class = t_class+"_bottom";
-          }
-          arrow_top = -12;
-          marg_top = Math.round(top + org_height + opts.edgeOffset);
-        }
-
-        if (t_class == "_right_top" || t_class == "_left_top") {
-          marg_top = marg_top + 5;
-        }
-        else if (t_class == "_right_bottom" || t_class == "_left_bottom") {
-          marg_top = marg_top - 5;
-        }
-
-        if (t_class == "_left_top" || t_class == "_left_bottom") {
-          marg_left = marg_left + 5;
-        }
-
-        tiptip_arrow.css({"margin-left": arrow_left+"px", "margin-top": arrow_top+"px"});
-        tiptip_holder.css({"margin-left": marg_left+"px", "margin-top": marg_top+"px"}).attr("class","tip"+t_class);
-
-        if (timeout) {
-          clearTimeout(timeout);
-        }
-        timeout = setTimeout(function() {
-          tiptip_holder.stop(true,true).fadeIn(opts.fadeIn);
-          return false;
-        }, opts.delay);
-
-        return false;
-      },
-      mouseout: function() {
-        opts.exit.call(this);
-        if (timeout) {
-          clearTimeout(timeout);
-        }
-
-        tiptip_holder.fadeOut(opts.fadeOut);
-        return false;
-      }
-    });
-
-    return false;
-  }
-
-  return false;
 })(jQuery);
 
 /* END jQuery Tooltip #1 */
