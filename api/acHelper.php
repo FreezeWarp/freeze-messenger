0b28b10d6ae4af266bef42c07265c4f0266fbb09
Joseph Parsons <josephtparsons@gmail.com>
2014-03-24 04:59:41 -0500

    Add js/acHelper.php, which stands for "autocomplete helper." This file should use excessive caching, but for now decides to skip that.

---
+++ api/acHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+$apiRequest = true;
+
+require('../global.php');
+
+/* Get Request Data */
+$request = fim_sanitizeGPC('g', array(
+  'list' => array(
+    'valid' => array(
+      'users', 'rooms',
+    ),
+    'require' => true,
+  ),
+
+  'search' => array(
+    'cast' => 'string',
+    'require' => true,
+  ),
+));
+
+
+switch ($request['list']) {
+
+  case 'users':
+    $entries = $slaveDatabase->getUsers(array(
+      'userNameSearch' => $request['search'],
+    ))->getColumnValues('userName');
+    break;
+
+  case 'rooms':
+    $entries = $slaveDatabase->getRooms(array(
+      'roomNameSearch' => $request['search'],
+    ))->getColumnValues('roomName');
+     break;
+
+}var_dump($entries); die();
+
+
+
+/* Data Predefine */
+$xmlData = array(
+  'editRoom' => array(
+    'activeUser' => array(
+      'userId' => (int) $user['userId'],
+      'userName' => $user['userName'],
+    ),
+    'errStr' => $errStr,
+    'errDesc' => $errDesc,
+    'entries' => $entries,
+  ),
+);
+
+
+/* Output Data */
+echo fim_outputApi($xmlData);
+?>
