ceb5cbda6b143063f0d301de1464da580a52c97e
Joseph T. Parsons <josephtparsons@gmail.com>
2016-09-29 21:51:01 -0500

    Add the FIMDatabase OAuth Storage library
    Currently doesn't implement a handful of things, and may not ever.

---
+++ functions/oauth2-server-php/src/OAuth2/Storage/FIMDatabaseOAuth.php
@@ -0,0 +1,267 @@
+<?php
+/**
+ * FIMDatabase implementation of OAuth
+ *
+ * @author Joseph Parsons
+ */
+class FIMDatabaseOAuth implements
+    AuthorizationCodeInterface,
+    AccessTokenInterface,
+    ClientCredentialsInterface,
+    UserCredentialsInterface,
+    RefreshTokenInterface,
+    UserClaimsInterface,
+    OpenIDAuthorizationCodeInterface
+{
+    protected $db;
+    protected $config;
+
+    const CLIENT_TABLE_FIELDS = 'client_id, client_secret, redirect_uri, grant_types, scope, user_id';
+    const ACCESS_TOKEN_TABLE_FIELDS = 'access_token, client_id, user_id, expires, scope';
+    const AUTHORIZATION_CODE_TABLE_FIELDS = 'authorization_code, client_id, user_id, redirect_uri, expires, scope, id_token';
+    const REFRESH_TOKEN_TABLE_FIELDS = 'refresh_token, client_id, user_id, expires, scope';
+
+    public function __construct($db, $config = array())
+    {
+        if (!$db instanceof \FIMDatabase) {
+            throw new Exception('First argument to OAuth2\Storage\FIMDatabase must be an instance of FIMDatabase');
+        }
+
+        $this->db = $db;
+        $this->config = array_merge(array(
+            'client_table' => $db->connectionInformation['tablePrefix'] . 'oauth_clients',
+            'access_token_table' => $db->connectionInformation['tablePrefix'] . 'oauth_access_tokens',
+            'refresh_token_table' => $db->connectionInformation['tablePrefix'] . 'oauth_refresh_tokens',
+            'code_table' => $db->connectionInformation['tablePrefix'] . 'oauth_authorization_codes',
+            'user_table' => $db->connectionInformation['tablePrefix'] . 'oauth_users',
+            'jwt_table'  => $db->connectionInformation['tablePrefix'] . 'oauth_jwt',
+            'jti_table'  => $db->connectionInformation['tablePrefix'] . 'oauth_jti',
+            'scope_table'  => $db->connectionInformation['tablePrefix'] . 'oauth_scopes',
+            'public_key_table'  => $db->connectionInformation['tablePrefix'] . 'oauth_public_keys',
+        ), $config);
+    }
+
+    /* OAuth2\Storage\ClientCredentialsInterface */
+    public function checkClientCredentials($client_id, $client_secret = null)
+    {
+        $result = $this->db->where(array('client_id' => $client_id))->select(array($this->config['client_table'] => self::CLIENT_TABLE_FIELDS))->getAsArray(false);
+
+        // make this extensible
+        return $result && $result['client_secret'] == $client_secret;
+    }
+
+    public function isPublicClient($client_id)
+    {
+
+        $result = $this->db->where(array('client_id' => $client_id))->select(array($this->config['client_table'] => self::CLIENT_TABLE_FIELDS))->getAsArray(false);
+
+        if (!length($result)) {
+            return false;
+        }
+
+        return empty($result['client_secret']);
+    }
+
+    /* OAuth2\Storage\ClientInterface */
+    public function getClientDetails($client_id)
+    {
+        return $this->db->where(array('client_id' => $client_id))->select(array($this->config['client_table'] => self::CLIENT_TABLE_FIELDS))->getAsArray(false);
+    }
+
+    public function setClientDetails($client_id, $client_secret = null, $redirect_uri = null, $grant_types = null, $scope = null, $user_id = null)
+    {
+        return $this->db->upsert($this->config['client_table'], array(
+            'client_id' => $client_id
+        ), array(
+            'client_secret' => $client_secret,
+            'redirect_uri' => $redirect_uri,
+            'grant_types' => $grant_types,
+            'scope' => $scope,
+            'user_id' => $user_id
+        ));
+    }
+
+    public function checkRestrictedGrantType($client_id, $grant_type)
+    {
+        $details = $this->getClientDetails($client_id);
+        if (isset($details['grant_types'])) {
+            $grant_types = explode(' ', $details['grant_types']);
+
+            return in_array($grant_type, (array) $grant_types);
+        }
+
+        // if grant_types are not defined, then none are restricted
+        return true;
+    }
+
+    /* OAuth2\Storage\AccessTokenInterface */
+    public function getAccessToken($access_token)
+    {
+        // Note: the original PDO implementation converted expires into a timestamp here as well. The FIMDatabase layer stores/fetches _all_ dates as timestamps (for reasons like this), so the conversion is unecessary.
+        return $this->db->where(array('access_token' => $access_token))->select(array($this->config['access_token_table'] => self::ACCESS_TOKEN_TABLE_FIELDS))->getAsArray(false);
+    }
+
+    public function setAccessToken($access_token, $client_id, $user_id, $expires, $scope = null)
+    {
+        return $this->db->upsert($this->config['access_token_table'], array(
+            'access_token' => $access_token
+        ), array(
+            'client_id' => $client_id,
+            'expires' => $expires,
+            'user_id' => $user_id,
+            'scope' => $scope
+        ));
+    }
+
+    public function unsetAccessToken($access_token)
+    {
+        return $this->db->delete($this->config['access_token_table'], array(
+            'access_token' => $access_token
+        ));
+    }
+
+    /* OAuth2\Storage\AuthorizationCodeInterface */
+    public function getAuthorizationCode($code)
+    {
+        // Note: the original PDO implementation converted expires into a timestamp here as well. The FIMDatabase layer stores/fetches _all_ dates as timestamps (for reasons like this), so the conversion is unecessary.
+        return $this->db->where(array('authorization_code' => $code))->select(array($this->config['code_table'] => self::AUTHORIZATION_CODE_TABLE_FIELDS))->getAsArray(false);
+    }
+
+    public function setAuthorizationCode($code, $client_id, $user_id, $redirect_uri, $expires, $scope = null, $id_token = null)
+    {
+        if (func_num_args() > 6) { // ...Wait, why?
+            // we are calling with an id token
+            return call_user_func_array(array($this, 'setAuthorizationCodeWithIdToken'), func_get_args());
+        }
+
+
+        return $this->db->upsert($this->config['code_table'], array(
+            'authorization_code' => $code
+        ), array(
+            'client_id' => $client_id,
+            'user_id' => $user_id,
+            'redirect_uri' => $redirect_uri,
+            'expires' => $expires,
+            'scope' => $scope
+        ));
+    }
+
+    /* I don't know why this function exists. */
+    private function setAuthorizationCodeWithIdToken($code, $client_id, $user_id, $redirect_uri, $expires, $scope = null, $id_token = null)
+    {
+        return $this->db->upsert($this->config['code_table'], array(
+            'authorization_code' => $code
+        ), array(
+            'client_id' => $client_id,
+            'user_id' => $user_id,
+            'redirect_uri' => $redirect_uri,
+            'expires' => $expires,
+            'scope' => $scope,
+            'id_token' => $id_token,
+        ));
+    }
+
+    public function expireAuthorizationCode($code)
+    {
+        return $this->db->delete($this->config['code_table'], array(
+            'access_token' => $code
+        ));
+    }
+
+    /* OAuth2\Storage\UserCredentialsInterface */
+    public function checkUserCredentials($username, $password)
+    {
+        if ($user = $this->getUser($username)) {
+            return $this->checkPassword($user, $password);
+        }
+
+        return false;
+    }
+
+    public function getUserDetails($username)
+    {
+        return $this->getUser($username);
+    }
+
+    /* UserClaimsInterface */
+    public function getUserClaims($user_id, $claims)
+    {
+        if (!$userDetails = $this->getUserDetails($user_id)) {
+            return false;
+        }
+
+        $claims = explode(' ', trim($claims));
+        $userClaims = array();
+
+        // for each requested claim, if the user has the claim, set it in the response
+        $validClaims = explode(' ', self::VALID_CLAIMS);
+        foreach ($validClaims as $validClaim) {
+            if (in_array($validClaim, $claims)) {
+                if ($validClaim == 'address') {
+                    // address is an object with subfields
+                    $userClaims['address'] = $this->getUserClaim($validClaim, $userDetails['address'] ?: $userDetails);
+                } else {
+                    $userClaims = array_merge($userClaims, $this->getUserClaim($validClaim, $userDetails));
+                }
+            }
+        }
+
+        return $userClaims;
+    }
+
+    protected function getUserClaim($claim, $userDetails)
+    {
+        $userClaims = array();
+        $claimValuesString = constant(sprintf('self::%s_CLAIM_VALUES', strtoupper($claim)));
+        $claimValues = explode(' ', $claimValuesString);
+
+        foreach ($claimValues as $value) {
+            $userClaims[$value] = isset($userDetails[$value]) ? $userDetails[$value] : null;
+        }
+
+        return $userClaims;
+    }
+
+    /* OAuth2\Storage\RefreshTokenInterface */
+    public function getRefreshToken($refresh_token)
+    {
+        return $this->db->where(array('refresh_token' => $refresh_token))->select(array($this->config['refresh_token_table'] => self::REFRESH_TOKEN_TABLE_FIELDS))->getAsArray(false);
+    }
+
+    public function setRefreshToken($refresh_token, $client_id, $user_id, $expires, $scope = null)
+    {
+        // convert expires to datestring
+        $expires = date('Y-m-d H:i:s', $expires);
+
+        return $this->db->insert($this->config['refresh_token_table'], array(
+            'refresh_token' => $refresh_token,
+            'client_id' => $client_id,
+            'user_id' => $user_id,
+            'expires' => $expires,
+            'scope' => $scope,
+        ));
+    }
+
+    public function unsetRefreshToken($refresh_token)
+    {
+        return $this->db->delete($this->config['refresh_token_table'], array(
+            'refresh_token' => $refresh_token,
+        ));
+    }
+
+    protected function checkPassword($user, $password)
+    {
+        return $user->checkPassword($password);
+    }
+
+    public function getUser($username)
+    {
+        require('../../../../fim_user.php');
+
+        return $this->db->getUsers(array(
+            'userNames' => array($username),
+            'includePasswords' => true
+        ))->getAsUser();
+    }
+}
+?>
