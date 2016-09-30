059004a75e15596875de04dc98645acc3988b9e8
Joseph T. Parsons <josephtparsons@gmail.com>
2016-09-30 07:53:13 -0500

    The complete overhaul of fim_user
    It's hard to say how well it's working, but it should make future
    caching a lot easier.

--- functions/fim_user.php
+++ functions/fim_user.php
@@ -1,204 +1,266 @@
 <?php
-class fimUser {
-  public $id;
-  public $name;
-  public $socialGroupIds;
-  public $mainGroupId;
-  public $parentalFlags;
-  public $parentalAge;
-  public $anonId;
-  public $privs;
 
-  protected $userData;
-  protected $resolved;
+class fimUser
+{
+    public $id;
+    private $name;
+    private $socialGroupIds;
+    private $mainGroupId;
+    private $parentalFlags;
+    private $parentalAge;
+    private $privs;
+    private $lastSync;
+    private $avatar;
+    private $userNameFormat;
+    private $messageFormatting;
+    private $defaultRoomId;
+    private $profile;
+
+    private $anonId;
+
+    protected $generalCache;
+    protected $userData;
+
+    private $resolved = array();
+    private $userDataConversion = array(
+        'userId' => 'id',
+        'userName' => 'name',
+        'userNameFormat' => 'userNameFormat',
+        'userGroupId' => 'mainGroupId',
+        'socialGroupIds' => 'socialGroupIds',
+        'userParentalFlags' => 'parentalFlags',
+        'userParentalAge' => 'parentalAge',
+        'privs' => 'privs',
+        'lastSync' => 'lastSync',
+        'messageFormatting' => 'messageFormatting',
+        'defaultRoomId' => 'defaultRoomId',
+        'profile' => 'profile',
+        'avatar' => 'avatar',
+        'options' => 'options',
+    );
+
+    private $userDataPullGroups = array(
+        'userId, userName, privs, lastSync',
+        'userGroupId, socialGroupIds, userParentalFlags, userParentalAge',
+        'messageFormatting, profile, avatar',
+        'options, defaultRoomId',
+    );
 
 
-  /**
-   * @param $roomData mixed Should either be an array or an integer (other values will simply fail to populate the object's data). If an array, should correspond with a row obtained from the `rooms` database, if an integer should correspond with the room ID.
-   */
-  public function __construct($userData) {
-    if (is_int($userData)) $this->id = $userData;
-    elseif (is_array($userData)) $this->populateFromArray($userData); // TODO: test contents
-    elseif ($userData === false) $this->id = false;
-    else throw new Exception('Invalid room data specified -- must either be an associative array corresponding to a table row, a user ID, or false (to create a user, etc.)');
+    /**
+     * @param $roomData mixed Should either be an array or an integer (other values will simply fail to populate the object's data). If an array, should correspond with a row obtained from the `rooms` database, if an integer should correspond with the room ID.
+     */
+    public function __construct($userData)
+    {
+        global $generalCache;
+        $this->generalCache = $generalCache;
 
-    $this->userData = $userData;
-  }
+        $this->avatar = $this->generalCache->getConfig('avatarDefault');
+        $this->userNameFormat = $this->generalCache->getConfig('userNameFormat');
+        $this->defaultRoomId = $this->generalCache->getConfig('defaultRoomId');
 
 
-  public function __get($property) {
-    if (in_array($property, array('', '', '', ''))) {
 
+        if (is_int($userData))
+            $this->id = $userData;
+
+        elseif (is_array($userData))
+            $this->populateFromArray($userData); // TODO: test contents
+
+        elseif ($userData === false)
+            $this->id = false;
+
+        else
+            throw new Exception('Invalid user data specified -- must either be an associative array corresponding to a table row, a user ID, or false (to create a user, etc.)');
+
+        $this->userData = $userData;
     }
-  }
 
 
-  function checkPassword($password) {
-    /* TODO: IP limit to calling this function. */
+    public function __get($property) {
+        global $database;
 
-    switch ($this->userData['passwordFormat']) {
-      case 'phpass':
-        if (!isset($this->userData['passwordHash'])) {
-          throw new Exception('User object was not generated with password hash information.');
+        if (!in_array($property, $this->resolved)) {
+            if (!$this->id) throw new Exception('Uninitialised user object.');
+
+            // Find selection group
+
+            if (isset(array_flip($this->userDataConversion)[$property])) {
+                echo $needle = array_flip($this->userDataConversion)[$property];
+                $selectionGroup = array_values(array_filter($this->userDataPullGroups, function ($var) use ($needle) {
+                    return strpos($var, $needle) !== false;
+                }))[0];
+
+//                var_dump($selectionGroup);
+//                var_dump($needle); var_dump($property); die();
+
+                $this->populateFromArray($database->where(array('userId' => $this->id))->select(array($database->sqlPrefix . 'users' => $selectionGroup . ', userId'))->getAsArray(false));
+            }
+        }
+
+        return $this->$property;
+    }
+
+
+    /*
+     * The first time data is loaded,
+     */
+    public function __set($property, $value)
+    {
+        global $config, $loginConfig;
+        $this->{$property} = $value;
+
+        // If we've already processed the value once, it generally won't need to be reprocessed. Permissions, for instance, may be altered intentionally. We do make an exception for setting integers to what should be arrays -- we will reprocess in this case.
+        if (!in_array($property, $this->resolved) ||
+            (($property === 'parentalFlags' || $property === 'socialGroupIds')
+            && gettype($value) === 'integer')
+        ) {
+            $this->resolved[] = $property;
+
+
+            // Social Group IDs: Convert CSV to Array
+            if ($property = 'socialGroupIds')
+                $this->socialGroupIds = explode(',', $value);
+
+
+            // Parental Flags: Convert CSV to Array, or empty if disabled
+            else if ($property === 'parentalFlags') {
+                if ($config['parentalEnabled']) $this->parentalFlags = explode(',', $value);
+                else                              $this->parentalFlags = array();
+            }
+
+
+            // Parental Age: Disable if feature is disabled.
+            else if ($property = 'parentalAge') {
+                if ($config['parentalEnabled']) $this->parentalAge = $value;
+                else                              $this->parentalAge = 255;
+            }
+
+
+            // Priviledges: modify based on global permissions, inconsistencies, and superuser status.
+            else if ($property = 'privs') {
+                // If certain features are disabled, remove user priviledges. The bitfields should be maintained, however, for when a feature is reenabled.
+                if (!$this->generalCache->getConfig('userRoomCreation'))
+                    $this->privs &= ~USER_PRIV_CREATE_ROOMS;
+                if (!$this->generalCache->getConfig('userPrivateRoomCreation'))
+                    $this->privs &= ~(USER_PRIV_PRIVATE_ALL | USER_PRIV_PRIVATE_FRIENDS); // Note: does not disable the usage of existing private rooms. Use "privateRoomsEnabled" for this.
+                if ($this->generalCache->getConfig('disableTopic'))
+                    $this->privs &= ~USER_PRIV_TOPIC; // Topics are disabled (in fact, this one should also disable the returning of topics; TODO).
+
+                // Certain bits imply other bits. Make sure that these are consistent.
+                if ($this->privs & USER_PRIV_PRIVATE_ALL)
+                    $this->privs |= USER_PRIV_PRIVATE_FRIENDS;
+
+                // Superuser override (note that any user with GRANT or in the $config superuser array is automatically given all permissions, and is marked as protected. The only way, normally, to remove a user's GRANT status, because they are automatically protected, is to do so directly in the database.)
+                if (in_array($this->id, $loginConfig['superUsers']) || ($this->privs & ADMIN_GRANT))
+                    $this->privs = 0x7FFFFFFF;
+                elseif ($this->privs & ADMIN_ROOMS)
+                    $this->privs |= (USER_PRIV_VIEW | USER_PRIV_POST | USER_PRIV_TOPIC); // Being a super-moderator grants a user the ability to view, post, and make topic changes in all rooms.
+            }
+
+            else if ($property = 'anonId') {
+                if ($this->id === $this->generalCache->getConfig('anonymousUserId')) {
+                    $this->name .= $this->anonId;
+                }
+            }
+        }
+    }
+
+
+    function checkPassword($password)
+    {
+        global $database;
+
+        if ($database->lockoutActive()) {
+
         }
         else {
-          require_once('PasswordHash.php');
+            $database->lockoutIncrement();
+            /* TODO: IP limit to calling this function. */
 
-          $h = new PasswordHash(8, FALSE);
+            switch ($this->userData['passwordFormat']) {
+                case 'phpass':
+                    if (!isset($this->userData['passwordHash'])) {
+                        throw new Exception('User object was not generated with password hash information.');
+                    } else {
+                        require_once('PasswordHash.php');
 
-          return $h->CheckPassword($password, $this->userData['passwordHash']);
+                        $h = new PasswordHash(8, FALSE);
+
+                        return $h->CheckPassword($password, $this->userData['passwordHash']);
+                    }
+                    break;
+
+                case 'vbmd5':
+                    if (!isset($this->userData['passwordHash'], $this->userData['passwordSalt'])) {
+                        throw new Exception('User object was not generated with password hash information.');
+                    } else {
+                        return ($this->userData['passwordHash'] === md5(md5($password) . $this->userData['passwordSalt']));
+                    }
+                    break;
+
+                case 'raw':
+                    return ($this->userData['passwordHash'] === $password);
+                    break;
+
+                default:
+                    throw new Exception('Invalid password format.');
+                    break;
+            }
+
+            return false;
         }
-        break;
+    }
 
-      case 'vbmd5':
-        if (!isset($this->userData['passwordHash'], $this->userData['passwordSalt'])) {
-          throw new Exception('User object was not generated with password hash information.');
+
+    private function populateFromArray($userData)
+    {
+        $this->resolved = array_diff($this->resolved, array_values($userData)); // The resolution process in __set modifies the data based from an array in several ways. As a result, if we're importing from an array a second time, we either need to ignore the new value or, as in this case, uncheck the resolve[] entries to have them reparsed when __set fires.
+
+        foreach ($userData AS $attribute => $value)
+            $this->__set($this->userDataConversion[$attribute], $value);
+    }
+
+    /* Do I even remember what this was going to be for? Not really. */
+    public function syncUser()
+    {
+        global $database;
+
+        if ($this->lastSync >= (time() - $this->generalCache->getConfig('userSyncThreshold'))) { // This updates various caches every so often. In general, it is a rather slow process, and as such does tend to take a rather long time (that is, compared to normal - it won't exceed 500 miliseconds, really).
+            $database->updateUserCaches($this); // TODO
         }
-        else {
-          return ($this->userData['passwordHash'] === md5(md5($password) . $this->userData['passwordSalt']));
+    }
+
+
+    /**
+     * Modify or create a user.
+     * @internal The row will be set to a merge of roomDefaults->existingRow->specifiedOptions.
+     *
+     * @param $options - Corresponds mostly with room columns, though the options tag is seperted.
+     *
+     * @return bool|resource
+     */
+    public function set($options, $create = false)
+    {
+        global $database;
+
+        if (isset($options['password'])) {
+            require 'PasswordHash.php';
+            $h = new PasswordHash(8, FALSE);
+            $options['passwordHash'] = $h->HashPassword($options['password']);
+            $options['passwordFormat'] = 'phpass';
+
+            unset($options['password']);
         }
-        break;
 
-      case 'raw':
-        return ($this->userData['passwordHash'] === $password);
-        break;
-
-      default:
-        throw new Exception('Invalid password format.');
-        break;
+        if ($this->id) {
+            return $database->upsert($database->sqlPrefix . "users", array(
+                'userId' => $this->id,
+            ), $options);
+        } else {
+            return $database->insert($database->sqlPrefix . "users", $options);
+        }
     }
-
-    return false;
-  }
-
-
-
-  // TODO: Resolve only certain data.
-  public function resolve() {
-    global $database;
-
-    if (!$this->id) return false; // If no ID is present, return false;
-    elseif (!$this->resolved) {
-      return $this->populateFromArray($database->getUsers(array(
-        'userIds' => array($this->id)
-      ))->getAsArray(false));
-    }
-    else return true;
-  }
-
-
-
-  private function populateFromArray($userData) {
-    global $loginConfig, $generalCache, $config; // $config is todo
-
-    /* Make sure userData contains neccessary information. */
-    if (!count($userData)
-        || !is_array($userData)
-        || !isset($userData['userId'], $userData['userName'], $userData['privs'])
-        || !array_key_exists('userParentalFlags', $userData)
-        || !array_key_exists('userParentalAge', $userData)) throw new Exception("Invalid userData: " . print_r($userData));
-
-
-    if (!$userData['lastSync'] <= (time() - $config['userSyncThreshold'])) { // This updates various caches every so often. In general, it is a rather slow process, and as such does tend to take a rather long time (that is, compared to normal - it won't exceed 500 miliseconds, really).
-      // $userData = $database->updateUserCaches(); // TODO
-    }
-
-
-    /* Set Basic Information */
-    $this->id = (int) $userData['userId'];
-    $this->name = $userData['userName'];
-    $this->privs = (int) $userData['privs'];
-    
-    
-    /* Priviledges */
-    // If certain features are disabled, remove user priviledges. The bitfields should be maintained, however, for when a feature is reenabled.
-    if (!$generalCache->getConfig('userRoomCreation')) $this->privs &= ~USER_PRIV_CREATE_ROOMS;
-    if (!$generalCache->getConfig('userPrivateRoomCreation')) $this->privs &= ~(USER_PRIV_PRIVATE_ALL | USER_PRIV_PRIVATE_FRIENDS); // Note: does not disable the usage of existing private rooms. Use "privateRoomsEnabled" for this.
-    if ($generalCache->getConfig('disableTopic')) $this->privs &= ~USER_PRIV_TOPIC; // Topics are disabled (in fact, this one should also disable the returning of topics; TODO).
-
-    // Certain bits imply other bits. Make sure that these are consistent.
-    if ($this->privs & USER_PRIV_PRIVATE_ALL) $this->privs |= USER_PRIV_PRIVATE_FRIENDS;
-
-    // Superuser override (note that any user with GRANT or in the $config superuser array is automatically given all permissions, and is marked as protected. The only way, normally, to remove a user's GRANT status, because they are automatically protected, is to do so directly in the database.)
-    if (in_array($this->id, $loginConfig['superUsers']) || ($this->privs & ADMIN_GRANT)) $this->privs = 0x7FFFFFFF;
-    elseif ($this->privs & ADMIN_ROOMS) $this->privs |= (USER_PRIV_VIEW | USER_PRIV_POST | USER_PRIV_TOPIC); // Being a super-moderator grants a user the ability to view, post, and make topic changes in all rooms.
-
-
-    /* Anon User Information */
-    if ($this->id === $generalCache->getConfig('anonymousUserId')) {
-      $this->anonId = rand($generalCache->getConfig('anonymousUserMinId'), $generalCache->getConfig('anonymousUserMaxId'));
-      $this->name .= $this->anonId;
-    }
-
-
-    /* Set Username Formatting */
-    $this->avatar = $userData['avatar'] ?: $generalCache->getConfig('avatarDefault');
-    $this->nameFormat = $userData['userNameFormat'] ?: $generalCache->getConfig('userNameFormat');
-
-
-    /* Set Default Formatting */
-    $this->messageFormatting = $userData['messageFormatting'];
-
-
-    /* Set Misc Information */
-    $this->defaultRoomId = $userData['defaultRoomId'] ?: $generalCache->getConfig('defaultRoomId');
-    $this->profile = $userData['profile'];
-
-
-    /* Handle Groups */
-    $this->socialGroupIds = explode(',', $userData['socialGroupIds']);
-    $this->mainGroupId = $userData['userGroupId'];
-
-
-    /* Set Parental Information */
-    if ($config['parentalEnabled']) {
-      $this->parentalFlags = explode(',', $userData['userParentalFlags']);
-      $this->parentalAge = $userData['userParentalAge'];
-    }
-    else {
-      $this->parentalFlags = array();
-      $this->parentalAge = 255;
-    }
-
-
-    /* Mark Resolved */
-    $this->resolved = true;
-
-
-    /* Return True */
-    return true;
-  }
-
-
-
-  /**
-   * Modify or create a room.
-   * @internal The row will be set to a merge of roomDefaults->existingRow->specifiedOptions.
-   *
-   * @param $options - Corresponds mostly with room columns, though the options tag is seperted.
-   *
-   * @return bool|resource
-   */
-  public function set($options, $create = false) {
-    global $database;
-
-    if (isset($options['password'])) {
-      require 'PasswordHash.php';
-      $h = new PasswordHash(8, FALSE);
-      $options['passwordHash'] = $h->HashPassword($options['password']);
-      $options['passwordFormat'] = 'phpass';
-
-      unset($options['password']);
-    }
-
-    if ($this->id) {
-      return $database->upsert($database->sqlPrefix . "users", array(
-        'userId' => $this->id,
-      ), $options);
-    }
-    else {
-      return $database->insert($database->sqlPrefix . "users", $options);
-    }
-  }
 }
+
 ?>
