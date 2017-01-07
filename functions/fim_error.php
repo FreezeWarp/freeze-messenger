e83af87cfd08c3e19ba436e71b6292b535f4ff22
Joseph T. Parsons <josephtparsons@gmail.com>
2017-01-07 00:04:10 -0600

    fimError: allow not dying immediately

--- functions/fim_error.php
+++ functions/fim_error.php
@@ -6,17 +6,17 @@
  * Note: fimError can not be caught. It is intended to display data to the API.
  */
 class fimError extends Exception {
-  public function __construct($code = false, $string = false, $context = array()) {
+  public function __construct($code = false, $string = false, $context = array(), $return = false) {
     global $config;
 
     $this->email = $config['email'];
     $this->displayBacktrace = $config['displayBacktrace'];
 
-    if ($code) $this->trigger($code, $string, $context);
+    if ($code) $this->trigger($code, $string, $context, $return);
   }
 
 
-  public function trigger($code, $string = '', $context = array()) {
+  public function trigger($code, $string = '', $context = array(), $return = false) {
     ob_end_clean(); // Clean the output buffer and end it. This means that when we show the error in a second, there won't be anything else with it.
     header('HTTP/1.1 500 Internal Server Error'); // When an exception is encountered, we throw an error to tell the server that the software effectively is broken.
 
@@ -36,9 +36,15 @@ class fimError extends Exception {
     }
 
 
-
-    die(new apiData(array(
-      'exception' => $errorData,
-    )));
+    if ($return) {
+        return array(
+            'exception' => $errorData,
+        );
+    }
+    else {
+        die(new apiData(array(
+          'exception' => $errorData,
+        )));
+    }
   }
 }
