48ed24a8ed53fd0d811a5beec8732c333123886d
Joseph T. Parsons <josephtparsons@gmail.com>
2017-01-07 01:08:10 -0600

    Revert "Revert "Update global with cache changes, which I forgot to commit""
    This reverts commit ba9f798e550edd58d681391676c4b6a0710f812b.

--- global.php
+++ global.php
@@ -126,6 +126,11 @@ define("ADMIN_USERS", 0x100000);
 define("ADMIN_FILES", 0x400000);
 define("ADMIN_CENSOR", 0x1000000);
 
+define("POST_FORMAT_BOLD", 0x1);
+define("POST_FORMAT_ITALICS", 0x2);
+define("POST_FORMAT_BOLD", 0x1);
+define("POST_FORMAT_BOLD", 0x1);
+
 
 
 
@@ -221,11 +226,10 @@ unset($dbConnect); // There is no reason the login credentials should still be a
 $generalCache = new fimCache($cacheConnect['servers'], $cacheConnect['driver'], $database, $slaveDatabase);
 
 // Get Configuration Data
-$config = $generalCache->getConfig();
-$database->setConfig($config);
-
-
+$config = new fimConfig($cacheConnect['servers'], $cacheConnect['driver'], $database, $slaveDatabase);
 $database->queryLogToFile = ($config['logQueries'] ? $config['logQueriesFile'] : false);
+
+
 ////* User Login (Requires Database) *////
 require_once(dirname(__FILE__) . '/validate.php'); // This is where all the user validation stuff occurs.
 ?>
