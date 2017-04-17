6e17349fc946e60ca672a18e9a841317e15a3909
Joseph Parsons <josephtparsons@gmail.com>
2017-04-17 17:44:34 -0500

    getServerStatus: indent to 4 spaces

--- api/getServerStatus.php
+++ api/getServerStatus.php
@@ -21,7 +21,7 @@
  * @version 3.0
  * @author Jospeph T. Parsons <josephtparsons@gmail.com>
  * @copyright Joseph T. Parsons 2014
-*/
+ */
 
 $apiRequest = true;
 $ignoreLogin = true;
@@ -31,137 +31,138 @@ require('../global.php');
 
 /* Emoticons */
 switch($loginConfig['method']) {
-  case 'vbulletin3':
-  case 'vbulletin4':
-    $smilies = $integrationDatabase->select(array(
-      "{$forumTablePrefix}smilie" => 'smilietext emoticonText, smiliepath emoticonFile',
-    ))->getAsArray(true);
-    break;
+    case 'vbulletin3':
+    case 'vbulletin4':
+        $smilies = $integrationDatabase->select(array(
+                                                    "{$forumTablePrefix}smilie" => 'smilietext emoticonText, smiliepath emoticonFile',
+                                                ))->getAsArray(true);
+        break;
 
-  case 'phpbb':
-    $smilies = $integrationDatabase->select(array(
-      "{$forumTablePrefix}smilies" => 'code emoticonText, smiley_url emoticonFile'
-    ))->getAsArray(true);
-    break;
+    case 'phpbb':
+        $smilies = $integrationDatabase->select(array(
+                                                    "{$forumTablePrefix}smilies" => 'code emoticonText, smiley_url emoticonFile'
+                                                ))->getAsArray(true);
+        break;
 
-  case 'vanilla':
-    // TODO: Convert
-    $smilies = $database->select(array(
-      $database->sqlPrefix . "emoticons" => 'emoticonText, emoticonFile'
-    ))->getAsArray(true);
-    break;
+    case 'vanilla':
+        // TODO: Convert
+        $smilies = $database->select(array(
+                                         $database->sqlPrefix . "emoticons" => 'emoticonText, emoticonFile'
+                                     ))->getAsArray(true);
+        break;
 
-  default:
-    $smilies = array();
-    break;
+    default:
+        $smilies = array();
+        break;
 }
 
 
 
 if (count($smilies)) {
-  switch ($loginConfig['method']) {
-    case 'phpbb':
-      $forumUrlS = $loginConfig['url'] . 'images/smilies/';
-      break;
+    switch ($loginConfig['method']) {
+        case 'phpbb':
+            $forumUrlS = $loginConfig['url'] . 'images/smilies/';
+            break;
 
-    case 'vanilla':
-      $forumUrlS = $installUrl;
-      break;
+        case 'vanilla':
+            $forumUrlS = $installUrl;
+            break;
 
-    case 'vbulletin3':
-    case 'vbulletin4':
-      $forumUrlS = $loginConfig['url'];
-      break;
-  }
+        case 'vbulletin3':
+        case 'vbulletin4':
+            $forumUrlS = $loginConfig['url'];
+            break;
+    }
 
-  foreach ($smilies AS $smilie) {
-    $smilies2[$smilie['emoticonText']] = $forumUrlS . $smilie['emoticonFile'];
-  }
+    foreach ($smilies AS $smilie) {
+        $smilies2[$smilie['emoticonText']] = $forumUrlS . $smilie['emoticonFile'];
+    }
 }
 
 
-$apiData = new apiData(array(
-    'serverStatus' => array(
-      'activeUser' => array(
-        'userId' => $user->id,
-        'userName' => $user->name,
-      ),
+echo new apiData(
+    [
+        'serverStatus' => array(
+            'activeUser' => array(
+                'userId' => $user->id,
+                'userName' => $user->name,
+            ),
 
-      'fim_version' => FIM_VERSION,
-      'installedPlugins' => array(),
-      'installUrl' => $installUrl,
+            'fim_version' => FIM_VERSION,
+            'installedPlugins' => array(),
+            'installUrl' => $installUrl,
 
-      'parentalControls' => array(
-        'parentalEnabled' => $config['parentalEnabled'],
-        'parentalForced' => $config['parentalForced'],
-        'parentalAgeChangeable' => $config['parentalAgeChangeable'],
-        'parentalRegistrationAge' => $config['parentalRegistrationAge'],
-        'parentalFlags' => new apiOutputList($config['parentalFlags']),
-        'parentalAges' => new apiOutputList($config['parentalAges']),
-        'enableCensor' => $config['censorEnabled'],
-      ),
+            'parentalControls' => array(
+                'parentalEnabled' => $config['parentalEnabled'],
+                'parentalForced' => $config['parentalForced'],
+                'parentalAgeChangeable' => $config['parentalAgeChangeable'],
+                'parentalRegistrationAge' => $config['parentalRegistrationAge'],
+                'parentalFlags' => new apiOutputList($config['parentalFlags']),
+                'parentalAges' => new apiOutputList($config['parentalAges']),
+                'enableCensor' => $config['censorEnabled'],
+            ),
 
-      'branding' => array(
-        'forumType' => $loginConfig['method'],
-        'forumUrl' => $loginConfig['url'],
-      ),
+            'branding' => array(
+                'forumType' => $loginConfig['method'],
+                'forumUrl' => $loginConfig['url'],
+            ),
 
-      'requestMethods' => array(
-        'poll' => true,
-        'serverSentEvents' => (bool) $config['serverSentEvents'],
-      ),
+            'requestMethods' => array(
+                'poll' => true,
+                'serverSentEvents' => (bool) $config['serverSentEvents'],
+            ),
 
-      'registrationPolicies' => array(
-        'ageRequired' => (bool) $config['ageRequired'],
-        'ageMinimum' => (int) $config['ageMinimum'],
-        'emailRequired' => (bool) $config['emailRequired'],
-      ),
+            'registrationPolicies' => array(
+                'ageRequired' => (bool) $config['ageRequired'],
+                'ageMinimum' => (int) $config['ageMinimum'],
+                'emailRequired' => (bool) $config['emailRequired'],
+            ),
 
-      'fileUploads' => array(
-        'enabled' => (bool) $config['enableUploads'],
-        'generalEnabled' => (bool) $config['enableGeneralUploads'],
-        'maxAll' => (int) $config['uploadMaxFiles'],
-        'maxUser' => (int) $config['uploadMaxUserFiles'],
-        'chunkSize' => (int) $config['fileUploadChunkSize'],
-        'orphanFiles' => (bool) $config['allowOrphanFiles'],
-        'allowedExtensions' => new apiOutputList($config['allowedExtensions']),
-        'mimes' => new apiOutputList($config['uploadMimes']),
-        'extensionChanges' => $config['extensionChanges'],
-        'fileContainers' => $config['fileContainers'],
-        'fileProofs' => $config['uploadMimeProof'],
-        'sizeLimits' => $config['uploadSizeLimits'],
-      ),
+            'fileUploads' => array(
+                'enabled' => (bool) $config['enableUploads'],
+                'generalEnabled' => (bool) $config['enableGeneralUploads'],
+                'maxAll' => (int) $config['uploadMaxFiles'],
+                'maxUser' => (int) $config['uploadMaxUserFiles'],
+                'chunkSize' => (int) $config['fileUploadChunkSize'],
+                'orphanFiles' => (bool) $config['allowOrphanFiles'],
+                'allowedExtensions' => new apiOutputList($config['allowedExtensions']),
+                'mimes' => new apiOutputList($config['uploadMimes']),
+                'extensionChanges' => $config['extensionChanges'],
+                'fileContainers' => $config['fileContainers'],
+                'fileProofs' => $config['uploadMimeProof'],
+                'sizeLimits' => $config['uploadSizeLimits'],
+            ),
 
-      'rooms' => array(
-        'roomLengthMinimum' => (int) $config['roomLengthMinimum'],
-        'roomLengthMaximum' => (int) $config['roomLengthMaximum'],
-        'disableTopic' => (bool) $config['disableTopic'],
-        'officialRooms' => (bool) $config['officialRooms'],
-        'hiddenRooms' => (bool) $config['hiddenRooms'],
-      ),
+            'rooms' => array(
+                'roomLengthMinimum' => (int) $config['roomLengthMinimum'],
+                'roomLengthMaximum' => (int) $config['roomLengthMaximum'],
+                'disableTopic' => (bool) $config['disableTopic'],
+                'officialRooms' => (bool) $config['officialRooms'],
+                'hiddenRooms' => (bool) $config['hiddenRooms'],
+            ),
 
-      'defaultFormatting' => array(
-        'color' => $config['defaultFormattingColor'],
-        'font' => $config['defaultFormattingFont'],
-        'highlight' => $config['defaultFormattingHighlight'],
-        'bold' => $config['defaultFormattingBold'],
-        'italics' => $config['defaultFormattingItalics'],
-        'underline' => $config['defaultFormattingUnderline'],
-        'strikethrough' => $config['defaultFormattingStrikethrough'],
-        'overline' => $config['defaultFormattingOverline'],
-      ),
+            'defaultFormatting' => array(
+                'color' => $config['defaultFormattingColor'],
+                'font' => $config['defaultFormattingFont'],
+                'highlight' => $config['defaultFormattingHighlight'],
+                'bold' => $config['defaultFormattingBold'],
+                'italics' => $config['defaultFormattingItalics'],
+                'underline' => $config['defaultFormattingUnderline'],
+                'strikethrough' => $config['defaultFormattingStrikethrough'],
+                'overline' => $config['defaultFormattingOverline'],
+            ),
 
-      'formatting' => array(
-        'fonts' => $config['fonts'],
-        'emoticons' => $smilies2,
-      ),
+            'formatting' => array(
+                'fonts' => $config['fonts'],
+                'emoticons' => $smilies2,
+            ),
 
-      'cacheDelays' => array(
-        'censorLists' => $config['censorBlackWhiteListsCacheRefresh'],
-      ),
+            'cacheDelays' => array(
+                'censorLists' => $config['censorBlackWhiteListsCacheRefresh'],
+            ),
 
-      'phpVersion' => (float) phpversion(), // We won't display the full version as it could pose an unneccessary security risk. We will, however, display the base version.
-    ),
-));
-echo $apiData;
+            'phpVersion' => (float) phpversion(), // We won't display the full version as it could pose an unneccessary security risk. We will, however, display the base version.
+        ),
+    ]
+);
 ?>
