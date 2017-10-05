c2c0a5793d4e21353a74f68b2d037e099e74892e
Joseph Parsons <josephtparsons@gmail.com>
2017-10-05 16:22:14 -0500

    Login: vBulletin 3 now sets user groups, social groups, nameFormat, bio, and smilies.

--- functions/Login/Database/LoginVbulletin3.php
+++ functions/Login/Database/LoginVbulletin3.php
@@ -57,12 +57,12 @@ class LoginVbulletin3 extends LoginDatabase {
     }
 
     public function setUser() {
-        global $loginConfig;
+        global $loginConfig, $database;
 
-        $vbUser = $this->loginFactory->database->select(array(
-            $this->loginFactory->database->sqlPrefix . $this->tables['users'] => $this->fields['users']
-        ), [
-            "name" => $_REQUEST['username']
+        $vbUser = $this->loginFactory->database->select([
+            "{$this->loginFactory->database->sqlPrefix}user" => 'userid, username, password, salt, displaygroupid, usergroupid, membergroupids, email, joindate, usertitle, posts, lastvisit',
+        ], [
+            'username' => $_REQUEST['username']
         ])->getAsArray(false);
 
         if (!$vbUser) {
@@ -72,31 +72,127 @@ class LoginVbulletin3 extends LoginDatabase {
             new \fimError('passwordInvalid', 'A user by the given password does not exist.');
         }
         else {
+            /* TODO: all of the fancy stuff here (querying the group tables) should be put on a timer, and only done once every so often. */
+
+            /* Get All User Groups */
+            $vbUser['displaygroupid'] = $vbUser['displaygroupid'] ?: $vbUser['usergroupid'];
+
+            $vbUserGroupIds = explode(',', $vbUser['membergroupids']);
+            if (!in_array($vbUser['usergroupid'], $vbUserGroupIds))
+                $vbUserGroupIds[] = $vbUser['usergroupid'];
+            if (!in_array($vbUser['displaygroupid'], $vbUserGroupIds))
+                $vbUserGroupIds[] = $vbUser['displaygroupid'];
+
+            $vbUserGroups = $this->loginFactory->database->select([
+                "{$this->loginFactory->database->sqlPrefix}usergroup" => 'usergroupid, title, opentag'
+            ], [
+                'usergroupid' => $this->loginFactory->database->in(fim_arrayValidate($vbUserGroupIds, 'int'))
+            ])->getAsArray('usergroupid');
+
+
+            /* Get Display Group */
+            if (isset($vbUserGroups[$vbUser['displaygroupid']])) {
+                /*
+                 * This isn't perfect, since it will not correctly detect escaped quotes inside of the style tag. However, it will detect pretttty much everything else.
+                 * (Obviously, the inherent style of the tags used will be ignored. It's a compromise.)
+                 */
+                preg_match_all("/style=('|\")(.+?)\\1/", $vbUserGroups[$vbUser['displaygroupid']]['opentag'], $matches);
+                $css = implode(';', $matches[2]);
+            }
+            else {
+                $css = '';
+            }
+
+
+            /* Create User */
             $this->loginFactory->user = new \fimUser([
                 'integrationMethod' => 'vb34',
-                'integrationId' => $vbUser['id'],
+                'integrationId' => $vbUser['userid'],
             ]);
             $this->loginFactory->user->resolveAll();
             $this->loginFactory->user->setDatabase([
                 'integrationMethod' => 'vb34',
-                'integrationId' => $vbUser['id'],
-                'profile' => "{$loginConfig['url']}member.php?u={$vbUser['id']}",
-                'name' => $vbUser['name'],
+                'integrationId' => $vbUser['userid'],
+                'profile' => "{$loginConfig['url']}member.php?u={$vbUser['userid']}",
+                'name' => $vbUser['username'],
                 'email' => $vbUser['email'],
-                'mainGroupId' => $vbUser['mainGroupId'],
-                'joinDate' => $vbUser['joinDate'],
-                'avatar' => "{$loginConfig['url']}/image.php?u={$vbUser['id']}",
+                'bio' => $vbUser['usertitle'],
+                'nameFormat' => $css,
+                'mainGroupId' => $vbUser['usergroupid'],
+                'joinDate' => $vbUser['joindate'],
+                'avatar' => "{$loginConfig['url']}/image.php?u={$vbUser['userid']}", // TODO, I think
             ]);
 
-            /**
-             * TODO:
-             * nameFormat from groupId
-             * check avatar
-             *
+
+            /* Get Social Groups The User Belongs To */
+            $vbSocialGroupIds = $this->loginFactory->database->select([
+                "{$this->loginFactory->database->sqlPrefix}socialgroupmember" => 'userid, groupid, type'
+            ], [
+                'userid' => $vbUser['userid'],
+                'type' => 'member'
+            ])->getColumnValues('groupid');
+
+            $vbSocialGroups = $this->loginFactory->database->select([
+                "{$this->loginFactory->database->sqlPrefix}socialgroup" => 'groupid, name'
+            ], [
+                'groupid' => $this->loginFactory->database->in($vbSocialGroupIds)
+            ])->getAsArray(true);
+
+
+            /* Create User Groups */
+            $groupNames = [];
+
+            $database->autoQueue(true);
+            foreach ($vbUserGroups AS $userGroup) {
+                $groupNames[] = $userGroup['title'];
+                @$database->createSocialGroup($userGroup['title']);
+            }
+
+            foreach ($vbSocialGroups AS $socialGroup) {
+                $groupNames[] = 'Social Group: ' . $socialGroup['name'];
+                @$database->createSocialGroup('Social Group: ' . $socialGroup['name'], $loginConfig['url'] . '/image.php?groupid=' . $socialGroup['groupid']);
+            }
+            @$database->autoQueue(false);
+
+
+            /* Join User Groups */
+            $dbGroupIds = $database->select([
+                'socialGroups' => 'id, name'
+            ], ['name' => $database->in($groupNames)])->getColumnValues('id');
+
+            $database->autoQueue(true);
+            foreach ($dbGroupIds AS $groupId) {
+                @$database->enterSocialGroup($groupId, $this->loginFactory->user);
+            }
+            @$database->autoQueue(false);
+
+
+
+            /*
+             * TODO: on timer
              */
+            $this->syncInstall();
 
             $this->oauthGrantType = $this->loginFactory->oauthGetIntegrationLogin();
         }
+    }
+
+    public function syncInstall() {
+        global $database, $loginConfig;
+
+        $smilies = $this->loginFactory->database->select(array(
+            "{$this->loginFactory->database->sqlPrefix}smilie" => 'smilietext emoticonText, smiliepath emoticonFile'
+        ))->getAsArray(true);
+        //var_dump($smilies); die();
+
+        $database->autoQueue(true);
+        foreach ($smilies AS $smilie) {
+            @$database->insert("{$database->sqlPrefix}emoticons", [
+                'emoticonText' => $smilie['emoticonText'],
+                'emoticonFile' => "{$loginConfig['url']}/{$smilie['emoticonFile']}"
+            ]);
+        }
+        @$database->autoQueue(false);
     }
 
 }
