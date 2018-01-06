9e6dc722a36f63cefe45640fc6bd89af136b90d7
Joseph Parsons <josephtparsons@gmail.com>
2018-01-06 00:34:41 -0600

    Fim\DatabaseParameters: Update for fimRoom refactor

--- functions/Fim/DatabaseParameters.php
+++ functions/Fim/DatabaseParameters.php
@@ -9,29 +9,29 @@ class DatabaseParameters
     public static function execute(DatabaseInstance $instance)
     {
         $instance->setTransformationParameters([
-            \Fim\Database::$sqlPrefix . 'files'              => ['roomIdLink' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
-            \Fim\Database::$sqlPrefix . 'messages'           => ['roomId' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
-            \Fim\Database::$sqlPrefix . 'messageFlood'       => ['roomId' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
-            \Fim\Database::$sqlPrefix . 'messageEditHistory' => ['roomId' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
-            \Fim\Database::$sqlPrefix . 'ping'               => ['roomId' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
+            \Fim\Database::$sqlPrefix . 'files'              => ['roomIdLink' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
+            \Fim\Database::$sqlPrefix . 'messages'           => ['roomId' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
+            \Fim\Database::$sqlPrefix . 'messageFlood'       => ['roomId' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
+            \Fim\Database::$sqlPrefix . 'messageEditHistory' => ['roomId' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
+            \Fim\Database::$sqlPrefix . 'ping'               => ['roomId' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
             \Fim\Database::$sqlPrefix . 'rooms'              => [
                 'nameSearchable' => ['\Fim\DatabaseInstance::makeSearchable', Type::string, null],
             ],
-            \Fim\Database::$sqlPrefix . 'roomStats'          => ['roomId' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
+            \Fim\Database::$sqlPrefix . 'roomStats'          => ['roomId' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
             \Fim\Database::$sqlPrefix . 'socialGroups'       => [
                 'memberUserIds' => ['\Fim\DatabaseInstance::packListCache', Type::blob, '\Fim\DatabaseInstance::unpackListCache']
             ],
-            \Fim\Database::$sqlPrefix . 'searchMessages'     => ['roomId' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
-            \Fim\Database::$sqlPrefix . 'searchCache'        => ['roomId' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
-            \Fim\Database::$sqlPrefix . 'unreadMessages'     => ['roomId' => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId']],
+            \Fim\Database::$sqlPrefix . 'searchMessages'     => ['roomId' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
+            \Fim\Database::$sqlPrefix . 'searchCache'        => ['roomId' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
+            \Fim\Database::$sqlPrefix . 'unreadMessages'     => ['roomId' => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId']],
             \Fim\Database::$sqlPrefix . 'users'              => [
                 'nameSearchable' => ['\Fim\DatabaseInstance::makeSearchable', Type::string, null],
-                'defaultRoomId'  => ['Fim\fimRoom::encodeId', Type::blob, 'Fim\fimRoom::decodeId'],
+                'defaultRoomId'  => ['\Fim\Room::encodeId', Type::blob, '\Fim\Room::decodeId'],
             ],
         ], [
             \Fim\Database::$sqlPrefix . 'rooms' => [
                 'name' => ['\Fim\DatabaseInstance::makeSearchable', Type::string, 'nameSearchable'],
-                'id'   => ['Fim\fimRoom::encodeId', Type::blob, 'idEncoded'],
+                'id'   => ['\Fim\Room::encodeId', Type::blob, 'idEncoded'],
             ],
             \Fim\Database::$sqlPrefix . 'users' => [
                 'name' => ['\Fim\DatabaseInstance::makeSearchable', Type::string, 'nameSearchable'],
